352ccd3ad28cf6439c31b9f00b2d6ac9ef607bde
shivam13juna <shivam13juna@gmail.com>
2019-08-18 16:25:08 +0530

    Setup is ready, but probably wont' work, let's see

--- data_provider/lanenet_data_processor_test.py
+++ data_provider/lanenet_data_processor_test.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time    : 18-5-11 下午4:58
-# @Author  : Luo Yao
-# @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
-# @File    : lanenet_data_processor.py
-# @IDE: PyCharm Community Edition
-"""
-实现LaneNet的数据解析类
-"""
+
 import tensorflow as tf
+import cv2
 
 from config import global_config
 
@@ -17,45 +9,29 @@ VGG_MEAN = [123.68, 116.779, 103.939]
 
 
 class DataSet(object):
-    """
-    实现数据集类
-    """
 
-    def __init__(self, dataset_info_file, batch_size):
+
+    def __init__(self, image, batch_size):
         """
+        dataset_info_file -> image
+        _img_list -> image
 
         :param dataset_info_file:
         """
-        self._dataset_info_file = dataset_info_file
+        # self._dataset_info_file = dataset_info_file
+        self.vidObj = cv2.VideoCapture(path) 
+  
+
+        self.success, self.image = self.vidObj.read() 
         self._batch_size = batch_size
-        self._img_list = self._init_dataset()
         self._next_batch_loop_count = 0
 
     def __len__(self):
         return self._len
 
-    def _init_dataset(self):
-        """
-        :return:
-        """
-        img_list = []
-
-        if not tf.gfile.Exists(self._dataset_info_file):
-            raise ValueError('Failed to find file: ' + self._dataset_info_file)
-
-        with open(self._dataset_info_file, 'r') as file:
-            for _info in file:
-                info_tmp = _info.strip(' ').split()
-                img_list.append(info_tmp[0][1:])
-
-        self._len = len(img_list)
-
-        return img_list
-
     @staticmethod
     def process_img(img_path):
-        img_raw = tf.read_file(img_path)
-        img_decoded = tf.image.decode_jpeg(img_raw, channels=3)
+        img_decoded = tf.image.decode_jpeg(img_path, channels=3)
         img_resized = tf.image.resize_images(img_decoded, [CFG.TRAIN.IMG_HEIGHT, CFG.TRAIN.IMG_WIDTH],
                                              method=tf.image.ResizeMethod.BICUBIC)
         img_casted = tf.cast(img_resized, tf.float32)
@@ -65,13 +41,8 @@ class DataSet(object):
         """
         :return:
         """
-
-        idx_start = self._batch_size * self._next_batch_loop_count
-        idx_end = self._batch_size * self._next_batch_loop_count + self._batch_size
-
-        if idx_end > len(self):
-            idx_end = len(self)
-
-        img_list = self._img_list[idx_start:idx_end]
-        self._next_batch_loop_count += 1
-        return img_list
+        self.success, self.image = self.vidObj.read() 
+        if self.success:
+            return img_list
+        else:
+            return 0
--- my_test.py
+++ my_test.py
@@ -27,12 +27,12 @@ def init_args():
     """
     parser = argparse.ArgumentParser()
     # parser.add_argument('--image_path', type=str, help='The image path or the src image save dir')
-    parser.add_argument('--vid_path', type=str, help='The path where the video is stored')
+    parser.add_argument('--vid_path',default='data/sample.mp4' type=str, help='The path where the video is stored')
 
-    parser.add_argument('--weights_path', type=str, help='The model weights path')
-    parser.add_argument('--is_batch', type=str, help='If test a batch of images', default='false')
-    parser.add_argument('--batch_size', type=int, help='The batch size of the test images', default=8)
-    parser.add_argument('--save_dir', type=str, help='Test result image save dir', default=None)
+    parser.add_argument('--weights_path', default='trained/', type=str, help='The model weights path')
+    parser.add_argument('--is_batch', type=str, help='If test a batch of images', default='True')
+    parser.add_argument('--batch_size', type=int, help='The batch size of the test images', default=1)
+    parser.add_argument('--save_dir', type=str, help='Test result image save dir', default='predicts/')
     parser.add_argument('--use_gpu', type=int, help='If use gpu set 1 or 0 instead', default=1)
 
     return parser.parse_args()
@@ -70,26 +70,24 @@ def test_lanenet(image_path, weights_path, use_gpu, batch_size, save_dir):
     with sess.as_default():
         sess.run(tf.global_variables_initializer())
         saver.restore(sess=sess, save_path=weights_path)
-        for i in range(math.ceil(len(image_list) / batch_size)):
-            print(i)
-            paths = test_dataset.next_batch()
-            instance_seg_image, existence_output = sess.run([binary_seg_ret, instance_seg_ret],
-                                                            feed_dict={input_tensor: paths})
-            for cnt, image_name in enumerate(paths):
-                print(image_name)
-                parent_path = os.path.dirname(image_name)
-                directory = os.path.join(save_dir, 'vgg_SCNN_DULR_w9', parent_path)
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-                file_exist = open(os.path.join(directory, os.path.basename(image_name)[:-3] + 'exist.txt'), 'w')
-                for cnt_img in range(4):
-                    cv2.imwrite(os.path.join(directory, os.path.basename(image_name)[:-4] + '_' + str(cnt_img + 1) + '_avg.png'),
-                            (instance_seg_image[cnt, :, :, cnt_img + 1] * 255).astype(int))
-                    if existence_output[cnt, cnt_img] > 0.5:
-                        file_exist.write('1 ')
-                    else:
-                        file_exist.write('0 ')
-                file_exist.close()
+    
+        paths = test_dataset.next_batch()
+        instance_seg_image, existence_output = sess.run([binary_seg_ret, instance_seg_ret],
+                                                        feed_dict={input_tensor: paths})
+        # for cnt, image_name in enumerate(paths):
+        #     print(image_name)
+        #     parent_path = os.path.dirname(image_name)
+        #     directory = os.path.join(save_dir, 'vgg_SCNN_DULR_w9', parent_path)
+        #     if not os.path.exists(directory):
+        #         os.makedirs(directory)
+        #     file_exist = open(os.path.join(directory, os.path.basename(image_name)[:-3] + 'exist.txt'), 'w')
+        for cnt_img in range(4):
+            cv2.imwrite(os.path.join(str(cnt_img + 1) + '_avg.png'),(instance_seg_image[cnt, :, :, cnt_img + 1] * 255).astype(int))
+        #     if existence_output[cnt, cnt_img] > 0.5:
+        #         file_exist.write('1 ')
+        #     else:
+        #         file_exist.write('0 ')
+        # file_exist.close()
     sess.close()
     return
 
